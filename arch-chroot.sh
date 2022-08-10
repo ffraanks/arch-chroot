1374a2fa36ded569a455bb44fb64bf6e7859bfd9
Franklin Souza <83979476+ffraanks@users.noreply.github.com>
2022-08-10 08:23:38 -0300

    Update arch-chroot.sh

--- arch-chroot.sh
+++ arch-chroot.sh
@@ -100,9 +100,9 @@ edit_sudoers(){
 }
 
 grub_install(){
-pacman -S grub efibootmgr --noconfirm
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ArchLinux --recheck
-grub-mkconfig -o /boot/grub/grub.cfg
+  pacman -S grub efibootmgr --noconfirm
+  grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ArchLinux --recheck
+  grub-mkconfig -o /boot/grub/grub.cfg
 }
 
 finish_install(){
