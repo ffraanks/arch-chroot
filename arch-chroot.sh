9b664c7d7c2c26760fdbeba2b2542cc7ff982604
Franklin Souza <83979476+ffraanks@users.noreply.github.com>
2022-08-11 16:44:38 -0300

    Update arch-chroot.sh

--- arch-chroot.sh
+++ arch-chroot.sh
@@ -63,6 +63,7 @@ pacman_config(){
   sed -i -r 's/^#(.*VerbosePkgLists.*)$/\1/' /etc/pacman.conf
   sed -i -r 's/^#(.*ParallelDownloads.*)$/\1/' /etc/pacman.conf
   sed -i '/ParallelDownloads/s/5/60/g' /etc/pacman.conf
+  sed -i '38s/$/ILoveCandy/' /etc/pacman.conf
   sed -i '93,94s/^#//' /etc/pacman.conf
   #echo ILoveCandy >> /etc/pacman.conf
 }
